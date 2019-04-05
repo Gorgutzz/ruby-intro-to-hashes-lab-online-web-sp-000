12b24e287b4eff319fe0e385601a627025e4b77e
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-05 22:28:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,10 @@ def monopoly_with_third_tier
 	monopoly = {:railroads => {
 		pieces: 4,
 		:names => {
-			read_railroad:
+			read_railroad: {},
+			pennsylvania_railroad: {},
+			b_and_c_railroad: {},
+			shortline: {}
 
 		}
 		:rent_in_dollars => {
