b25d4ae8cb3357a020d07b85b2b6b5a8367b270e
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-05 22:30:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,8 +34,7 @@ def monopoly_with_third_tier
 			pennsylvania_railroad: {},
 			b_and_c_railroad: {},
 			shortline: {}
-
-		}
+		},
 		rent_in_dollars: {
 			one_piece_owned: 25,
 			two_pieces_owned: 50,
